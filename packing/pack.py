2fa55c75da59a6e3a87593740ec55352d43cea5d
Jesper Duemose Nielsen <jesperdn@drcmr.dk>
2025-04-08 10:15:45 +0200

    Update packing script to accept an existing environment

--- packing/pack.py
+++ packing/pack.py
@@ -1,12 +1,12 @@
 import stat
 import glob
+import json
 import sys
 import argparse
 import os
 import subprocess
 import shutil
 import tempfile
-import argparse
 import re
 
 from setuptools_scm import get_version
@@ -50,8 +50,12 @@ def custom_version_func(version: ScmVersion) -> str:
     else:
         return version.format_with("{tag}")
 
-def build(simnibs_dist_dir, developer_id=None):
-
+def build(
+    env_name: str | None = None,
+    simnibs_wheel_dir: str | None = None,
+    # pack_dir: str | None = None,
+    macos_developer_id = None
+):
     # Import these here so I don't need to install them when running setup.py
     # The reason is that I need the two functions above in setup.py and here
     # but I don't want to duplicate them. So setup.py imports them from here.
@@ -65,79 +69,91 @@ def build(simnibs_dist_dir, developer_id=None):
     version = get_version(git_describe_command="git describe --tags --abbrev=0", version_scheme=custom_version_func)
 
     pack_dir = os.path.abspath('simnibs_installer')
-    env_prefix = os.path.join(pack_dir, 'simnibs_env_tmp')
-    simnibs_dist_dir = os.path.abspath(simnibs_dist_dir)
-
-    wheels = glob.glob(
-        os.path.join(simnibs_dist_dir, f'simnibs-{version}*.whl')
-    )
-    if len(wheels) == 0:
-        raise FileNotFoundError(f'Did not find any wheels for version {version}')
-
-    # Create temporary environment
     if os.path.isdir(pack_dir):
         shutil.rmtree(pack_dir)
 
-    if sys.platform == 'linux':
-        os_name = 'linux'
-    elif sys.platform == 'darwin':
-        os_name = 'macOS'
-    elif sys.platform == 'win32':
-        os_name = 'win'
-    else:
-        raise OSError('OS not supported!')
-    env = os.path.join(
-        simnibs_root_dir, f'environment_{os_name}.yml'
-    )
-    subprocess.run(
-        f'conda env create -p {env_prefix} -f {env} --yes',
-        check=True,
-        shell=True
-    )
-    if sys.platform == 'win32':
-        env_py = os.path.join(env_prefix, 'Scripts', 'python.exe')
-    else:
-        env_py = os.path.join(env_prefix, 'bin', 'python')
+    if env_name is None:
+        assert simnibs_wheel_dir is not None, "You must specify a path to simnibs wheels when using a temporary environment"
+        simnibs_wheel_dir = os.path.abspath(simnibs_wheel_dir)
+        wheels = glob.glob(
+            os.path.join(simnibs_wheel_dir, f'simnibs-{version}*.whl')
+        )
+        if len(wheels) == 0:
+            raise FileNotFoundError(f'Did not find any wheels for simnibs {version} in {simnibs_wheel_dir}')
 
-    # Install SimNIBS
-    subprocess.run(
-        f'{env_py} -m pip install simnibs=={version} --no-deps --no-index --find-links={simnibs_dist_dir}',
-        check=True,
-        shell=True
-    )
+        # Create temporary environment
+        env_is_temporary = True
+        env_prefix = os.path.join(pack_dir, 'simnibs_env_tmp')
 
-    # Pack
-    # I use .tar because MacOS erases the execute permission in .zip
+        if sys.platform == 'linux':
+            os_name = 'linux'
+        elif sys.platform == 'darwin':
+            os_name = 'macOS'
+        elif sys.platform == 'win32':
+            os_name = 'win'
+        else:
+            raise OSError('OS not supported!')
+        env_file = os.path.join(simnibs_root_dir, f'environment_{os_name}.yml')
+
+        print(f"Creating temporary environment from {env_file}")
+        subprocess.run(
+            f'conda env create -p {env_prefix} -f {env_file} -y',
+            check=True,
+            shell=True
+        )
+
+        print("Installing SimNIBS")
+        subprocess.run(
+            f'conda run -p {env_prefix} python -m pip install simnibs=={version} --no-deps --no-index --find-links={simnibs_wheel_dir}',
+            check=True,
+            shell=True
+        )
+
+    else:
+        # Use an existing environment
+        env_is_temporary = False
+        env_prefix = None
+        print(f"Using existing environment: {env_name}")
+
+        # check that simnibs is in the current environment
+        res = subprocess.run("conda list simnibs --json", shell=True,  capture_output=True)
+        assert len(json.loads(res.stdout)) == 1, f"SimNIBS is not installed in {env_name}"
+
+    print("Packing environment...")
+    # (use .tar because MacOS erases the execute permission in .zip)
+    packed_env_file = os.path.join(pack_dir, 'simnibs_env.tar')
+    packed_env_dir = os.path.join(pack_dir, 'simnibs_env')
     conda_pack.pack(
+        name=env_name,
         prefix=env_prefix,
-        output=os.path.join(pack_dir, 'simnibs_env.tar'),
+        output=packed_env_file,
         compress_level=0,
         force=True,
+        verbose=True,
         ignore_missing_files=True
     )
-    shutil.unpack_archive(
-        os.path.join(pack_dir, 'simnibs_env.tar'),
-        os.path.join(pack_dir, 'simnibs_env'),
-    )
-    os.remove(os.path.join(pack_dir, 'simnibs_env.tar'))
-    # Remove temporary env
-    subprocess.run(
-        f'conda env remove -y -p {env_prefix}',
-        check=True,
-        shell=True
-    )
 
-    # Copy documentation
+    print("Unpacking environment")
+    shutil.unpack_archive(packed_env_file, packed_env_dir,)
+    os.remove(packed_env_file)
+
+    print("Patching unpacked environment")
     shutil.copytree(
         os.path.join(simnibs_root_dir, 'docs', 'build', 'html'),
         os.path.join(pack_dir, 'documentation')
     )
-
-    # Copy the fix_entrypoints script and the postinstall script
     shutil.copy(
         os.path.join(simnibs_root_dir, 'packing', 'fix_entrypoints.py'),
-        os.path.join(pack_dir, 'simnibs_env')
+        packed_env_dir
     )
+
+    if env_is_temporary:
+        print("Removing temporary environment")
+        subprocess.run(
+            f'conda env remove -y -p {env_prefix}',
+            check=True,
+            shell=True
+        )
 
     # Create OS-specific installer
     if sys.platform == 'win32':
@@ -172,6 +188,7 @@ def build(simnibs_dist_dir, developer_id=None):
             'simnibs_installer_windows.exe'
         )
     if sys.platform == 'darwin':
+        installer_name = "simnibs_installer_macos.pkg"
         with tempfile.TemporaryDirectory() as tmpdir:
             for fn in glob.glob(os.path.join(simnibs_root_dir, 'packing', 'macOS_installer', '*')):
                 fn_out = os.path.join(tmpdir, os.path.basename(fn))
@@ -187,6 +204,7 @@ def build(simnibs_dist_dir, developer_id=None):
             # Workaroud for Notarization
             # Instead of signing all binaries, I zip the enironment with a password
             # The postinstall script will unzip it in the user's computer
+            print("Repacking...")
             orig_folder = os.path.abspath(os.curdir)
             os.chdir(pack_dir)
             subprocess.run([
@@ -206,13 +224,13 @@ def build(simnibs_dist_dir, developer_id=None):
                 '--scripts', tmpdir,
                 '--install-location',
                 '/Applications/SimNIBS-'+ '.'.join(version.split('.')[:2]),
-                os.path.join(tmpdir, 'simnibs_installer_macos.pkg')
+                os.path.join(tmpdir, installer_name)
                 ],
                 check=True,
             )
             print('Running productbuild')
-            if developer_id is not None:
-                sign = ['--sign', developer_id]
+            if macos_developer_id is not None:
+                sign = ['--sign', macos_developer_id]
             else:
                 sign = []
             subprocess.run([
@@ -220,7 +238,7 @@ def build(simnibs_dist_dir, developer_id=None):
                 '--distribution', os.path.join(tmpdir, 'Distribution'),
                 '--package-path', tmpdir,
                 '--resources', tmpdir,
-                'simnibs_installer_macos.pkg'
+                installer_name
                 ] + sign,
                 check=True
             )
@@ -239,9 +257,10 @@ def build(simnibs_dist_dir, developer_id=None):
             os.stat(fn_script).st_mode |
             stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH
         )
-        print('compressing')
+        print('Repacking...')
+        installer_name = "simnibs_installer_linux"
         shutil.make_archive(
-            'simnibs_installer_linux',
+            installer_name,
             'gztar',
             # I use root_dir and base_dir so that it decompresses into a folder called
             # simnibs_installer
@@ -249,12 +268,18 @@ def build(simnibs_dist_dir, developer_id=None):
             base_dir=os.path.relpath(pack_dir)
         )
 
+    # Remove the (temporary) packing dir
+    shutil.rmtree(pack_dir)
+
+    # print(f"Created installer {installer_file}")
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         prog="simnibs-pack",
-        description="Prepare a SimNIBS intaller"
+        description="Create SimNIBS installers."
     )
-    parser.add_argument("dist_dir", help="Directory with the SimNIBS wheels to be packed")
-    parser.add_argument("--developer-id", default=None, help="Developer ID for signing in MacOS, DOES NOT SUPPORT NOTARIZATION (optional)")
+    parser.add_argument("--env", default=None, help="Name of existing environment to use.")
+    parser.add_argument("--simnibs-wheel-dir", default=None, help="Directory with the SimNIBS wheels to be packed")
+    parser.add_argument("--macos-developer-id", default=None, help="Developer ID for signing in MacOS, DOES NOT SUPPORT NOTARIZATION (optional)")
     args = parser.parse_args(sys.argv[1:])
-    build(args.dist_dir, args.developer_id)
+    build(args.env, args.simnibs_wheel_dir, args.macos_developer_id)
