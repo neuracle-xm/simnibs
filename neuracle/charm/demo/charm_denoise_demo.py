efbfb4f112c5e72b5c17818939c281401c6adc2b
slc90 <506091625@qq.com>
2026-06-24 15:09:11 +0800

    fix: 修复charm/denoise这一步demo缺少文件夹的错误

--- neuracle/charm/demo/charm_denoise_demo.py
+++ neuracle/charm/demo/charm_denoise_demo.py
@@ -8,8 +8,8 @@ CHARM 步骤3: 图像降噪示例
 - T2_reg.nii.gz (如果存在)
 """
 
-from neuracle.charm.simnibs_logging import simnibs_charm_file_logging
 from neuracle.charm.denoise import denoise_inputs
+from neuracle.charm.simnibs_logging import simnibs_charm_file_logging
 from neuracle.logger import setup_logging
 from neuracle.utils.constants import DATA_ROOT, PROJECT_ROOT
 
@@ -33,11 +33,13 @@ def main():
     # 2. 如果 T1_denoised 不存在，对 T1 进行 SANLM 降噪
     # 3. 如果 T2_reg 存在且 T2_reg_denoised 不存在，对 T2 进行降噪
     # 输出:
-    # - T1fs_denoised.nii.gz
-    # - T2_reg_denoised.nii.gz (如果存在 T2)
+    # - segmentation/T1_denoised.nii.gz
+    # - segmentation/T2_reg_denoised.nii.gz (如果存在 T2)
     with simnibs_charm_file_logging(
         str(subject_dir), filename="simnibs_charm_denoise.log"
     ):
+        # denoise_inputs 使用 SimNIBS SubjectFiles 路径，去噪结果会写入 segmentation 目录。
+        (subject_dir / "segmentation").mkdir(parents=True, exist_ok=True)
         denoise_inputs(subject_dir=str(subject_dir))
 
     print("=" * 60)
